343bf77443fc6c1d45b0cdcc85474fb9825bc842
Yuuki Harano <masm+github@masm11.me>
2019-10-08 23:05:55 +0900

    experimental support of tab-bar.

--- src/pgtkterm.c
+++ src/pgtkterm.c
@@ -1826,7 +1826,11 @@ x_draw_image_relief (struct glyph_string *s)
   if (s->hl == DRAW_IMAGE_SUNKEN
       || s->hl == DRAW_IMAGE_RAISED)
     {
-      thick = tool_bar_button_relief >= 0 ? tool_bar_button_relief : DEFAULT_TOOL_BAR_BUTTON_RELIEF;
+      thick = (tab_bar_button_relief < 0
+	       ? DEFAULT_TAB_BAR_BUTTON_RELIEF
+	       : (tool_bar_button_relief < 0
+		  ? DEFAULT_TOOL_BAR_BUTTON_RELIEF
+		  : min (tool_bar_button_relief, 1000000)));
       raised_p = s->hl == DRAW_IMAGE_RAISED;
     }
   else
@@ -1839,6 +1843,19 @@ x_draw_image_relief (struct glyph_string *s)
   y1 = y + s->slice.height - 1;
 
   extra_x = extra_y = 0;
+  if (s->face->id == TAB_BAR_FACE_ID)
+    {
+      if (CONSP (Vtab_bar_button_margin)
+	  && FIXNUMP (XCAR (Vtab_bar_button_margin))
+	  && FIXNUMP (XCDR (Vtab_bar_button_margin)))
+	{
+	  extra_x = XFIXNUM (XCAR (Vtab_bar_button_margin));
+	  extra_y = XFIXNUM (XCDR (Vtab_bar_button_margin));
+	}
+      else if (FIXNUMP (Vtab_bar_button_margin))
+	extra_x = extra_y = XFIXNUM (Vtab_bar_button_margin);
+    }
+
   if (s->face->id == TOOL_BAR_FACE_ID)
     {
       if (CONSP (Vtool_bar_button_margin)
@@ -4567,6 +4584,7 @@ pgtk_create_terminal (struct pgtk_display_info *dpyinfo)
   terminal->menu_show_hook = pgtk_menu_show;
   terminal->activate_menubar_hook = pgtk_activate_menubar;
   terminal->popup_dialog_hook = pgtk_popup_dialog;
+  terminal->change_tab_bar_height_hook = x_change_tab_bar_height;
   terminal->set_vertical_scroll_bar_hook = pgtk_set_vertical_scroll_bar;
   terminal->set_horizontal_scroll_bar_hook = pgtk_set_horizontal_scroll_bar;
   terminal->condemn_scroll_bars_hook = pgtk_condemn_scroll_bars;
@@ -5569,7 +5587,7 @@ motion_notify_event(GtkWidget *widget, GdkEvent *event, gpointer *user_data)
 	{
 	  static Lisp_Object last_mouse_window;
 	  Lisp_Object window = window_from_coordinates
-	    (f, event->motion.x, event->motion.y, 0, false);
+	    (f, event->motion.x, event->motion.y, 0, false, false);
 
 	  /* A window will be autoselected only when it is not
 	     selected now and the last mouse movement event was
@@ -5689,6 +5707,7 @@ button_event(GtkWidget *widget, GdkEvent *event, gpointer *user_data)
 
   /* If we decide we want to generate an event to be seen
      by the rest of Emacs, we put it here.  */
+  bool tab_bar_p = false;
   bool tool_bar_p = false;
 
   EVENT_INIT (inev.ie);
@@ -5738,6 +5757,27 @@ button_event(GtkWidget *widget, GdkEvent *event, gpointer *user_data)
 
   if (f && xg_event_is_for_scrollbar (f, event))
     f = 0;
+
+  if (f)
+    {
+      /* Is this in the tab-bar?  */
+      if (WINDOWP (f->tab_bar_window)
+	  && WINDOW_TOTAL_LINES (XWINDOW (f->tab_bar_window)))
+	{
+	  Lisp_Object window;
+	  int x = event->button.x;
+	  int y = event->button.y;
+
+	  window = window_from_coordinates (f, x, y, 0, true, true);
+	  tab_bar_p = EQ (window, f->tab_bar_window);
+
+	  if (tab_bar_p && event->button.button < 4)
+	    handle_tab_bar_click
+	      (f, x, y, event->type == GDK_BUTTON_PRESS,
+	       pgtk_gtk_to_emacs_modifiers (dpyinfo, event->button.state));
+	}
+    }
+
   if (f)
     {
       if (!tool_bar_p)
